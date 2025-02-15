7f6617db2b689433796d2b7cc1152fe2d8791424
Juliognoatto <juliognoatto5@gmail.com>
2025-02-15 15:27:12 -0300

    add about html

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,3 +1,4 @@
+import { cardData } from "../../dataprojects"
 import styles from "../../styles/components/Projects.module.scss"
 
 function Projects() {
@@ -5,7 +6,29 @@ function Projects() {
   return(
 
     <section className={styles.Projects}>
+      <h2>Projetos</h2>
 
+      {cardData.map((card) => (
+        
+      
+      <div key={card.id} className={styles.ProjectsContainer}>
+
+        <h3>{card.title}</h3>     
+        
+        <div className={styles.ProjectsContent}>
+          <p>
+            {card.description}
+          </p>
+        
+          <img src="" alt="" />  
+        </div>
+        <ol>
+          <li> <link rel="stylesheet" href="" /> </li>
+          <li> <link rel="stylesheet" href="" /> </li>
+        </ol>
+      
+      </div>
+      ))}
     </section>
   )
 }
---
+++ src/dataprojects.ts
@@ -0,0 +1,27 @@
+export const cardData = [
+    {
+      id: 1,
+      title: "Card 1",
+      description: "Descrição do card 1",
+      imageUrl: "https://via.placeholder.com/150",
+      linkAlbum: "",
+      linkMateria: ""
+
+    },
+    {
+      id: 2,
+      title: "Card 2",
+      description: "Descrição do card 2",
+      imageUrl: "https://via.placeholder.com/150",
+      linkAlbum: "",
+      linkMateria: ""
+    },
+    {
+      id: 3,
+      title: "Card 3",
+      description: "Descrição do card 3",
+      imageUrl: "https://via.placeholder.com/150",
+      linkAlbum: "",
+      linkMateria: ""
+    }
+  ];
